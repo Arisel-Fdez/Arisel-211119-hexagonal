e364be99a5d15429a492611fcee6ba377ef571ab
Arisel Fernandez Cañaveral <84791183+Arisel-Fdez@users.noreply.github.com>
2023-09-25 04:43:51 -0600

    Delete src directory

--- src/book/infraestructure/bookRouter.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { addBookController } from "./dependencies";
-
-export const bookRouter = express.Router();
-
-bookRouter.post(
-    "/",
-    addBookController.run.bind(addBookController)
-);
