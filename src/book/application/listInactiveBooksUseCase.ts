e236082c3efce7b9b0f3eebc6ee77a282edfda5a
Arisel Fernandez Cañaveral <84791183+Arisel-Fdez@users.noreply.github.com>
2023-09-28 23:52:08 -0600

    Delete src directory

--- src/book/application/listInactiveBooksUseCase.ts
+++
@@ -1,9 +0,0 @@
-import { BookRepository } from '../domain/bookRepository';
-
-export class ListInactiveBooksUseCase {
-    constructor(readonly bookRepository: BookRepository) { }
-
-    async run() {
-        return await this.bookRepository.listInactiveBooks();
-    }
-}
